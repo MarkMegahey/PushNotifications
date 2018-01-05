d5c429b34c4d6006a8e82c8e4089893d1c29eeef
MarkMegahey <Mark@smartdriverclub.co.uk>
2018-01-05 16:17:25 +0000

    Moved Phone Tokens

--- PushNotifications/Program.cs
+++ PushNotifications/Program.cs
@@ -11,17 +11,18 @@ namespace PushNotifications
 {
     class Program
     {
-        public static string MarkS8 = "cGYJ24ElFHI:APA91bFfGQnHQqlVGuKuDW89enx6pV_1CTR0smhConeacGnKSvD_dumkxZW5vDQ3Ax3TxaWbcy3Kgjt_xdLJtXhCEYyCG2ubmPOyvO8mJ4cqmojQvYRmlGREz_pAuo0F3CycWGcXHSid";
-        public static string BenIphone6 = "839841809466B7AD1D8FDAD6608BB614595051FFC7B138A5F5503F7FBDC34117";
+        public static string MarkS8 = File.ReadAllText("C:\\GIT\\PushNotifications\\PushNotifications\\TestPhones\\MarkS8.txt");
+        public static string BenIphone6 = File.ReadAllText("C:\\GIT\\PushNotifications\\PushNotifications\\TestPhones\\BenIphone6.txt");
         public static string PushCertificate = "C:\\GIT\\PushNotifications\\PushNotifications\\PushCredentials\\Apple\\ios_development.p12";
         public static string PushCertificatePassword = File.ReadAllText("C:\\GIT\\PushNotifications\\PushNotifications\\PushCredentials\\Apple\\ios_development_password.txt");
-
-
 
         static void Main(string[] args)
         {
             bool CloseApp = false;
+            bool ValidSelection = false;
 
+            Console.WriteLine("PUSH NOTIFICATIONS");
+            Console.WriteLine(" ");
             Console.WriteLine("Please Select An Option To Continue");
             Console.WriteLine(" ");
             Console.WriteLine("1. Send Android Push Notification");
@@ -29,31 +30,118 @@ namespace PushNotifications
             Console.WriteLine(" ");
             Console.WriteLine("3. Exit The Application");
 
-
             while (CloseApp == false)
             {
+                ValidSelection = false;
                 var input = Console.ReadLine();
 
                 switch (input)
                 {
                     case "1":
                         {
-                            //Payload BasicPayload = new Payload { actionType = "none" };
-                            Message MessagePayload = new Message { actionType = "meassage", message = "This is a message" };
-                            //Warning WarningPayload = new Warning { actionType = "warning", vehicleId = "M2lQ" };
-                            //Deal DealPayload = new Deal { actionType = "deal" };
-                            //Url URLPayload = new Url { actionType = "url" , url = "http://smartdriverclub.co.uk" };
-                            PushNotification StandardNotification = new PushNotification { title = "New Notification", body = "Test Notification", payload = MessagePayload };
-
-
                             List<string> AndroidPushTokens = new List<string>();
                             AndroidPushTokens.Add(MarkS8);
 
+                            Console.WriteLine("Please Select An Payload Option To Continue");
+                            Console.WriteLine(" ");
+                            Console.WriteLine("1. BasicPayload");
+                            Console.WriteLine("2. MessagePayload");
+                            Console.WriteLine("3. WarningPayload");
+                            Console.WriteLine("4. DealPayload");
+                            Console.WriteLine("5. URLPayload");
+                            Console.WriteLine(" ");
+                            Console.WriteLine("6. Go Back");
 
-                            SendAndroidPushNotification(AndroidPushTokens, StandardNotification);
+                            while (ValidSelection == false)
+                            {
 
-                            Console.WriteLine("Android Push Sent");
-                            input = null;
+                                var payloadType = Console.ReadLine();
+
+                                switch (payloadType)
+                                {
+                                    case "1":
+                                        {
+                                            Payload BasicPayload = new Payload { actionType = "none" };
+                                            PushNotification StandardNotification = new PushNotification { title = "BasicPayload", body = "Test Notification", payload = BasicPayload };
+                                            SendAndroidPushNotification(AndroidPushTokens, StandardNotification);
+
+                                            ValidSelection = true;
+                                            Console.WriteLine("Android Push Sent");
+                                            input = null;
+                                            break;
+                                        }
+                                    case "2":
+                                        {
+                                            Message MessagePayload = new Message { actionType = "message", message = "This is a message" };
+                                            PushNotification StandardNotification = new PushNotification { title = "MessagePayload", body = "Test Notification", payload = MessagePayload };
+                                            SendAndroidPushNotification(AndroidPushTokens, StandardNotification);
+
+                                            ValidSelection = true;
+                                            Console.WriteLine("Android Push Sent");
+                                            input = null;
+                                            break;
+                                        }
+                                    case "3":
+                                        {
+                                            Warning WarningPayload = new Warning { actionType = "warning", vehicleId = "M2lQ" };
+                                            PushNotification StandardNotification = new PushNotification { title = "WarningPayload", body = "Test Notification", payload = WarningPayload };
+                                            SendAndroidPushNotification(AndroidPushTokens, StandardNotification);
+
+                                            ValidSelection = true;
+                                            Console.WriteLine("Android Push Sent");
+                                            input = null;
+                                            break;
+                                        }
+                                    case "4":
+                                        {
+                                            Deal DealPayload = new Deal { actionType = "deal" };
+                                            PushNotification StandardNotification = new PushNotification { title = "DealPayload", body = "Test Notification", payload = DealPayload };
+                                            SendAndroidPushNotification(AndroidPushTokens, StandardNotification);
+
+                                            ValidSelection = true;
+                                            Console.WriteLine("Android Push Sent");
+                                            input = null;
+                                            break;
+                                        }
+                                    case "5":
+                                        {
+                                            Url URLPayload = new Url { actionType = "url", url = "http://smartdriverclub.co.uk" };
+                                            PushNotification StandardNotification = new PushNotification { title = "URLPayload", body = "Test Notification", payload = URLPayload };
+                                            SendAndroidPushNotification(AndroidPushTokens, StandardNotification);
+
+                                            ValidSelection = true;
+                                            Console.WriteLine("Android Push Sent");
+                                            input = null;
+                                            break;
+                                        }
+                                    case "6":
+                                        {
+                                            Console.Clear();
+                                            Console.WriteLine("Please Select An Option To Continue");
+                                            Console.WriteLine(" ");
+                                            Console.WriteLine("1. Send Android Push Notification");
+                                            Console.WriteLine("2. Send Apple Push Notificaiton");
+                                            Console.WriteLine(" ");
+                                            Console.WriteLine("3. Exit The Application");
+                                            ValidSelection = true;
+                                            break;
+                                        }
+                                    default:
+                                        {
+                                            Console.Clear();
+                                            Console.WriteLine("Please Select An Payload Option To Continue");
+                                            Console.WriteLine(" ");
+                                            Console.WriteLine("1. BasicPayload");
+                                            Console.WriteLine("2. MessagePayload");
+                                            Console.WriteLine("3. WarningPayload");
+                                            Console.WriteLine("4. DealPayload");
+                                            Console.WriteLine("5. URLPayload");
+                                            Console.WriteLine(" ");
+                                            Console.WriteLine("6. Go Back");
+                                            break;
+                                        }
+                                }
+                            }
                             break;
                         }
                     case "2":
@@ -63,7 +151,7 @@ namespace PushNotifications
                             List<string> ApplePushTokens = new List<string>();
                             ApplePushTokens.Add(BenIphone6);
 
-                            SendApplePushNotification(ApplePushTokens, StandardNotification);
+                            //SendApplePushNotification(ApplePushTokens, StandardNotification);
 
                             Console.WriteLine("Apple Push Sent");
                             input = null;
@@ -77,7 +165,8 @@ namespace PushNotifications
                         }
                     default:
                         {
-                            Console.WriteLine("Incorrect Selection");
+                            Console.Clear();
+                            Console.WriteLine("Please Select An Option To Continue");
                             Console.WriteLine(" ");
                             Console.WriteLine("1. Send Android Push Notification");
                             Console.WriteLine("2. Send Apple Push Notificaiton");
@@ -105,13 +194,13 @@ namespace PushNotifications
                 aggregateEx.Handle(ex =>
                 {
 
-                    // See what kind of exception it was to further diagnose
-                    if (ex is GcmNotificationException)
+                // See what kind of exception it was to further diagnose
+                if (ex is GcmNotificationException)
                     {
                         var notificationException = (GcmNotificationException)ex;
 
-                        // Deal with the failed notification
-                        var gcmNotification = notificationException.Notification;
+                    // Deal with the failed notification
+                    var gcmNotification = notificationException.Notification;
                         var description = notificationException.Description;
 
                         Console.WriteLine($"GCM Notification Failed: ID={gcmNotification.MessageId}, Desc={description}");
@@ -145,23 +234,23 @@ namespace PushNotifications
 
                         if (!string.IsNullOrWhiteSpace(newId))
                         {
-                            // If this value isn't null, our subscription changed and we should update our database
-                            Console.WriteLine($"Device RegistrationId Changed To: {newId}");
+                        // If this value isn't null, our subscription changed and we should update our database
+                        Console.WriteLine($"Device RegistrationId Changed To: {newId}");
                         }
                     }
                     else if (ex is RetryAfterException)
                     {
                         var retryException = (RetryAfterException)ex;
-                        // If you get rate limited, you should stop sending messages until after the RetryAfterUtc date
-                        Console.WriteLine($"GCM Rate Limited, don't send more until after {retryException.RetryAfterUtc}");
+                    // If you get rate limited, you should stop sending messages until after the RetryAfterUtc date
+                    Console.WriteLine($"GCM Rate Limited, don't send more until after {retryException.RetryAfterUtc}");
                     }
                     else
                     {
                         Console.WriteLine("GCM Notification Failed for some unknown reason");
                     }
 
-                    // Mark it as handled
-                    return true;
+                // Mark it as handled
+                return true;
                 });
             };
 
@@ -207,13 +296,13 @@ namespace PushNotifications
                 aggregateEx.Handle(ex =>
                 {
 
-                    // See what kind of exception it was to further diagnose
-                    if (ex is ApnsNotificationException)
+                // See what kind of exception it was to further diagnose
+                if (ex is ApnsNotificationException)
                     {
                         var notificationException = (ApnsNotificationException)ex;
 
-                        // Deal with the failed notification
-                        var apnsNotification = notificationException.Notification;
+                    // Deal with the failed notification
+                    var apnsNotification = notificationException.Notification;
                         var statusCode = notificationException.ErrorStatusCode;
 
                         Console.WriteLine($"Apple Notification Failed: ID={apnsNotification.Identifier}, Code={statusCode}");
@@ -221,12 +310,12 @@ namespace PushNotifications
                     }
                     else
                     {
-                        // Inner exception might hold more useful information like an ApnsConnectionException			
-                        Console.WriteLine($"Apple Notification Failed for some unknown reason : {ex.InnerException}");
+                    // Inner exception might hold more useful information like an ApnsConnectionException			
+                    Console.WriteLine($"Apple Notification Failed for some unknown reason : {ex.InnerException}");
                     }
 
-                    // Mark it as handled
-                    return true;
+                // Mark it as handled
+                return true;
                 });
             };
 
@@ -254,12 +343,12 @@ namespace PushNotifications
             apnsBroker.Stop();
         }
 
-        public class PushNotification 
+        public class PushNotification
         {
             public string title { get; set; }
             public string body { get; set; }
             public Payload payload { get; set; }
-            
+
         }
 
         public class Payload
@@ -279,7 +368,7 @@ namespace PushNotifications
 
         public class Deal : Payload
         {
-            
+
         }
 
         public class Url : Payload
